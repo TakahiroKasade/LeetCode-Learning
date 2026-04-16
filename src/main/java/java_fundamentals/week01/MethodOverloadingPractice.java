45af3d7891c25ea96849e9cff15c6c2793808503
TakahiroKasade <sam11092013@gmail.com>
2026-04-16 16:45:45 +0800

    feat: 新增混合型別的 Method Overloading 練習 (String + int)

--- src/main/java/java_fundamentals/week01/MethodOverloadingPractice.java
+++ src/main/java/java_fundamentals/week01/MethodOverloadingPractice.java
@@ -6,7 +6,8 @@ package java_fundamentals.week01;
  * 1. 實作第一個 getMax(int a, int b)，回傳兩數中的最大值
  * 2. 實作第二個 getMax(int a, int b, int c)，回傳三數中的最大值
  * 3. 提示：在三數版本中，可以呼叫兩數版本的方法來簡化邏輯
- * 4. 在 main 方法中分別測試兩個版本，觀察 Java 如何選取正確的方法
+ * 4. 進階挑戰：實作 printInfo(String name) 與 printInfo(String name, int age) 的混合型別重載
+ * 5. 在 main 方法中測試上述所有版本
  * 
  * @author Takahiro
  * @date 2026/04/16 (提前開始週五進度)
@@ -21,6 +22,12 @@ public class MethodOverloadingPractice {
         // 2. 測試三數版本
         int result2 = getMax(50, 120, 100);
         System.out.println("三數最大值 (50, 120, 100): " + result2);
+        
+        String result3= printInfo("Sam");
+        System.out.println(result3);
+        
+        String result4= printInfo("Sam",20);
+        System.out.println(result4);
     }
 
     /**
@@ -43,5 +50,19 @@ public class MethodOverloadingPractice {
         int tempMax = getMax(a, b);
         // 拿冠軍去跟 c 進行最終對決
         return getMax(tempMax, c);
+    }
+    
+    /**
+     * 混合型別重載練習 1：印出姓名訊息
+     */
+    public static String printInfo(String name) {
+        return "你好，" + name;
+    }
+
+    /**
+     * 混合型別重載練習 2：印出姓名與年紀訊息
+     */
+    public static String printInfo(String name, int age) {
+        return name + " 今年 " + age + " 歲";
     }
 }
